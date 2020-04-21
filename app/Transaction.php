b3cb55ff43c21f16c048a0ad467ec7ca26fef8ac
sc-ashik <orionsami160@gmail.com>
2020-04-21 15:53:25 +0600

    timezone changed

--- app/Transaction.php
+++ app/Transaction.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
 
-    protected $fillable=['parking_id'];
+    protected $fillable=['parking_id','locked_at'];
 
     public function feeCategory(){
         return $this->parking->feeCategory();
